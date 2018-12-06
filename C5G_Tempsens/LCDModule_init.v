d84c6d8b234786606dd2e7f9eceebd1bac5359fa
saqata <15t5041e@shinshu-u.ac.jp>
2018-12-06 11:21:42 +0900

    complete coping

--- C5G_Tempsens/LCDModule_init.v
+++ C5G_Tempsens/LCDModule_init.v
@@ -3,20 +3,38 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module lcdctrl (
-   	clk,
-	reset,
-	lcd_e,
-	lcd_rs,
-	lcd_rw,
-	sf_d,
-    );
-    
-    input clk;
-    input reset;
+module LCDModule_init(
+	CLK, 			// in
+	LCD_RW,  		// out			
+	LCD_RS,			// out			 		 
+	LCD_E,  		// out			
+	LCD_DB,  		// out			
+	RDY, 			// out
+	DATA, 			// in
+	OPER, 			// in
+	ENB, 			// in
+	RST				// in
+);
+input CLK;			// For this code to work without modification, CLK should equal 24MHz
+input DATA;			// The Data to send to the LCD Module
+input OPER;			// The Type of operation to perform (data or instruction) 
+input ENB;			// Tells the module that the data is valid and start reading DATA and OPER
+input RST;
 
-    output lcd_e, lcd_rs, lcd_rw;
-    output [7:0] sf_d;
+output RDY;			// Indicates that the module is Idle and ready to take more data
+output LCD_RS, LCD_RW, LCD_E;
+output [7:0] LCD_DB;
+
+
+wire [7:0] DATA;
+wire [1:0] OPER;
+
+wire ENB;
+reg RDY;
+reg [7:0] LCD_DB=0;
+reg LCD_RW=0;			// always write to (and never read from) the LCD
+reg LCD_RS=0;			// HI means Data, LOW means Instruction/Command
+reg LCD_E=0;
 
 //===============================================================================================
 //------------------------------Define the Timing Parameters-------------------------------------
@@ -33,7 +51,7 @@ parameter [21:0] t_50000us	= 20'd2500000;	//50ms 		== ~2500000clks
 //===============================================================================================
 //------------------------------Define the BASIC Command Set-------------------------------------
 //===============================================================================================
-parameter [7:0] SETUP		= 8'b00111000;	//Execution time = 42us, sets to 8-bit interface, 2-line display, 5x7 dots
+parameter [7:0] SETUP		= 8'b00111100;	//Execution time = 42us, sets to 8-bit interface, 2-line display, 5x11 dots
 parameter [7:0] DISP_ON		= 8'b00001100;	//Execution time = 42us, Turn ON Display
 parameter [7:0] ALL_ON		= 8'b00001111;	//Execution time = 42us, Turn ON All Display
 parameter [7:0] ALL_OFF		= 8'b00001000;	//Execution time = 42us, Turn OFF All Display
@@ -132,9 +150,9 @@ always @(posedge CLK) begin
 			LCD_RW	<= 	1'b0;										//Indicate a write operation
 			LCD_E	<=	1'b0;										//We are in the initial setup, keep low until 250ns has past
 			LCD_DB 	<= 	8'b00000000;
-			RDY		<= 1'b0;										//Indicate that the module is busy
+			RDY		<= 	1'b0;										//Indicate that the module is busy
 			SUBSTATE	<=	0;
-			if(!flag_50000us) begin									//WAIT 40ms...worst case scenario
+			if(!flag_50000us) begin									//WAIT 50ms...worst case scenario
 				STATE				<=	STATE;						//Remain in current STATE
 				flag_rst			<=	1'b0; 						//Start or Continue counting				
 			end
@@ -144,3 +162,366 @@ always @(posedge CLK) begin
 			end		
 		end
 		//---------------------------------------------------------------------------------------
+		1: begin //-----------SET FUNCTION #1, 8-bit interface, 2-line display, 5x11 dots---------
+			LCD_RS				<=	1'b0;						//Indicate an instruction is to be sent soon
+			LCD_RW				<=	1'b0;						//Indicate a write operation
+			RDY					<= 	1'b0;						//Indicate that the module is busy
+			if(SUBSTATE==0)begin	 		
+                LCD_E				<=	1'b0;					//Disable Bus
+			    LCD_DB 				<=	LCD_DB;					//Maintain Previous Data on the Bus
+				STATE				<=	STATE;					
+				SUBSTATE			<=	1;
+			end			
+			if(SUBSTATE==1)begin				
+				LCD_E				<=	1'b1;					//Enable Bus		
+				LCD_DB 		    	<= SETUP;					//Data Valid
+				if(!flag_250ns) begin						    //WAIT at least 250ns (required for LCD_E)
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 				
+					SUBSTATE		<=	SUBSTATE+1;				//Go to next SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+			if(SUBSTATE==2)begin
+                LCD_E				<=	1'b0;					//Disable Bus, Triggers LCD to read BUS
+				LCD_DB 			    <= LCD_DB;					//Keep Data Valid
+				if(!flag_100us) begin						    //WAIT at least 100us (required for Initialization)
+					STATE			<=	STATE;					//Maintain current STATE
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 		
+					STATE			<=	STATE+1;				//Go to next STATE
+					SUBSTATE		<=	0;					    //Reset SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end	
+		end
+		2: begin //-----------SET FUNCTION #2, 8-bit interface, 2-line display, 5x11 dots---------
+			LCD_RS				<=	1'b0;						//Indicate an instruction is to be sent soon
+			LCD_RW				<=	1'b0;						//Indicate a write operation	
+			RDY					<=  1'b0;						//Indicate that the module is busy
+			if(SUBSTATE==0)begin	 		
+				LCD_E				<=	1'b0;					//Disable Bus
+				LCD_DB 			    <=	LCD_DB;					//Maintain Previous Data on the Bus
+				STATE				<=	STATE;				
+				SUBSTATE			<=	1;
+			end			
+			if(SUBSTATE==1)begin				
+				LCD_E				<=	1'b1;					//Enable Bus		
+				LCD_DB 			    <= SETUP;					//Data Valid
+				if(!flag_250ns) begin						    //WAIT at least 250ns (required for LCD_E)
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 				
+					SUBSTATE		<=	SUBSTATE+1;				//Go to next SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+			if(SUBSTATE==2)begin
+				LCD_E				<=	1'b0;					//Disable Bus, Triggers LCD to read BUS
+				LCD_DB 			    <= LCD_DB;					//Keep Data Valid
+				if(!flag_100us) begin						    //WAIT at least 100us (required for Initialization)
+					STATE			<=	STATE;					//Maintain current STATE
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 		
+					STATE			<=	STATE+1;				//Go to next STATE
+					SUBSTATE		<=	0;						//Reset SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end	
+		end
+		//---------------------------------------------------------------------------------------
+		3: begin //-----------------DISPLAY, Display OFF, Cursor OFF, Blinking OFF------------------
+			LCD_RS				<=	1'b0;						//Indicate an instruction is to be sent soon
+			LCD_RW				<=	1'b0;						//Indicate a write operation	
+			RDY					<= 1'b0;						//Indicate that the module is busy
+			if(SUBSTATE==0)begin	 		
+				LCD_E				<=	1'b0;					//Disable Bus
+				LCD_DB 				<=	LCD_DB;					//Maintain Previous Data on the Bus
+				STATE				<=	STATE;				
+				SUBSTATE			<=	1;
+			end			
+			if(SUBSTATE==1)begin				
+				LCD_E				<=	1'b1;					//Enable Bus		
+				LCD_DB 				<= ALL_OFF;					//Data Valid
+				if(!flag_250ns) begin							//WAIT at least 250ns (required for LCD_E)
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 				
+					SUBSTATE		<=	SUBSTATE+1;				//Go to next SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+			if(SUBSTATE==2)begin
+				LCD_E				<=	1'b0;					//Disable Bus, Triggers LCD to read BUS
+				LCD_DB 				<= LCD_DB;					//Keep Data Valid
+				if(!flag_42us) begin							//WAIT at least 42us (required for operation to process)
+					STATE			<=	STATE;					//Maintain current STATE
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 		
+					STATE			<=	STATE+1;				//Go to next STATE
+					SUBSTATE		<=	0;						//Reset SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+		end
+		//---------------------------------------------------------------------------------------
+		4: begin //-------------------DISPLAY CLEAR, clear the display screen--------------------
+			LCD_RS				<=	1'b0;						//Indicate an instruction is to be sent soon
+			LCD_RW				<=	1'b0;						//Indicate a write operation	
+			RDY					<= 	1'b0;						//Indicate that the module is busy
+			if(SUBSTATE==0)begin	 		
+				LCD_E				<=	1'b0;					//Disable Bus
+				LCD_DB 				<=	LCD_DB;					//Maintain Previous Data on the Bus
+				STATE				<=	STATE;				
+				SUBSTATE			<=	1;
+			end			
+			if(SUBSTATE==1)begin				
+				LCD_E				<=	1'b1;					//Enable Bus		
+				LCD_DB 				<= CLEAR;					//Data Valid
+				if(!flag_250ns) begin							//WAIT at least 250ns (required for LCD_E)
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 				
+					SUBSTATE		<=	SUBSTATE+1;				//Go to next SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+			if(SUBSTATE==2)begin
+				LCD_E				<=	1'b0;					//Disable Bus, Triggers LCD to read BUS
+				LCD_DB 				<= LCD_DB;					//Keep Data Valid
+				if(!flag_1640us) begin							//WAIT at least 1640us (required for operation to process)
+					STATE			<=	STATE;					//Maintain current STATE
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 		
+					STATE			<=	STATE+1;				//Go to next STATE
+					SUBSTATE		<=	0;						//Reset SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end			
+		end
+		//---------------------------------------------------------------------------------------
+		5: begin //---------Normal ENTRY, Cursor increments, Display is not shifted--------------
+			LCD_RS				<=	1'b0;						//Indicate an instruction is to be sent soon
+			LCD_RW				<=	1'b0;						//Indicate a write operation	
+			RDY					<= 	1'b0;						//Indicate that the module is busy
+			if(SUBSTATE==0)begin	 		
+				LCD_E				<=	1'b0;						//Disable Bus
+				LCD_DB 				<=	LCD_DB;					//Maintain Previous Data on the Bus
+				STATE				<=	STATE;				
+				SUBSTATE			<=	1;
+			end			
+			if(SUBSTATE==1)begin				
+				LCD_E				<=	1'b1;						//Enable Bus		
+				LCD_DB 				<= ENTRY_N;					//Data Valid
+				if(!flag_250ns) begin						//WAIT at least 250ns (required for LCD_E)
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 				
+					SUBSTATE		<=	SUBSTATE+1;				//Go to next SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+			if(SUBSTATE==2)begin
+				LCD_E				<=	1'b0;						//Disable Bus, Triggers LCD to read BUS
+				LCD_DB 				<= 	LCD_DB;					//Keep Data Valid
+				if(!flag_42us) begin							//WAIT at least 42us (required for operation to process)
+					STATE			<=	STATE;					//Maintain current STATE
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 		
+					STATE			<=	STATE+1;					//Go to next STATE
+					SUBSTATE		<=	0;							//Reset SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+		end
+		//---------------------------------------------------------------------------------------
+		6: begin //-----------------DISPLAY, Display ON, Cursor ON, Blinking ON------------------
+			LCD_RS				<=	1'b0;						//Indicate an instruction is to be sent soon
+			LCD_RW				<=	1'b0;						//Indicate a write operation	
+			RDY					<= 	1'b0;						//Indicate that the module is busy
+			if(SUBSTATE==0)begin	 		
+				LCD_E				<=	1'b0;					//Disable Bus
+				LCD_DB 				<=	LCD_DB;					//Maintain Previous Data on the Bus
+				STATE				<=	STATE;				
+				SUBSTATE			<=	1;
+			end			
+			if(SUBSTATE==1)begin				
+				LCD_E				<=	1'b1;					//Enable Bus		
+				LCD_DB 				<= 	ALL_ON;					//Data Valid
+				if(!flag_250ns) begin							//WAIT at least 250ns (required for LCD_E)
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 				
+					SUBSTATE		<=	SUBSTATE+1;				//Go to next SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+			if(SUBSTATE==2)begin
+				LCD_E				<=	1'b0;					//Disable Bus, Triggers LCD to read BUS
+				LCD_DB 			<= LCD_DB;						//Keep Data Valid
+				if(!flag_42us) begin							//WAIT at least 42us (required for operation to process)
+					STATE			<=	STATE;					//Maintain current STATE
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 		
+					STATE			<=	STATE+1;				//Go to next STATE
+					SUBSTATE		<=	0;						//Reset SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+		end
+		//---------------------------------------------------------------------------------------
+		7: begin //-------------------DISPLAY CLEAR, clear the display screen--------------------		
+			LCD_RS				<=	1'b0;						//Indicate an instruction is to be sent soon
+			LCD_RW				<=	1'b0;						//Indicate a write operation	
+			RDY					<= 	1'b0;						//Indicate that the module is busy
+			if(SUBSTATE==0)begin	 		
+				LCD_E				<=	1'b0;					//Disable Bus
+				LCD_DB 				<=	LCD_DB;					//Maintain Previous Data on the Bus
+				STATE				<=	STATE;				
+				SUBSTATE			<=	1;
+			end			
+			if(SUBSTATE==1)begin				
+				LCD_E				<=	1'b1;					//Enable Bus		
+				LCD_DB 				<= CLEAR;					//Data Valid
+				if(!flag_250ns) begin							//WAIT at least 250ns (required for LCD_E)
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 				
+					SUBSTATE		<=	SUBSTATE+1;				//Go to next SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+			if(SUBSTATE==2)begin
+				LCD_E				<=	1'b0;					//Disable Bus, Triggers LCD to read BUS
+				LCD_DB 				<= LCD_DB;					//Keep Data Valid
+				if(!flag_1640us) begin							//WAIT at least 1640us (required for operation to process)
+					STATE			<=	STATE;					//Maintain current STATE
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 		
+					STATE			<=	15;						//Go to next STATE
+					SUBSTATE		<=	0;						//Reset SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+		end
+		//---------------------------------------------------------------------------------------
+		9: begin//----------------------------- WRITE DATA -------------------------------------
+			LCD_RS				<=	1'b1;						//Indicate an instruction is to be sent soon
+			LCD_RW				<=	1'b0;						//Indicate a write operation	
+			RDY					<= 1'b0;						//Indicate that the module is busy
+			if(SUBSTATE==0)begin	 		
+				LCD_E				<=	1'b0;					//Disable Bus
+				LCD_DB 				<=	LCD_DB;					//Maintain Previous Data on the Bus
+				STATE				<=	STATE;				
+				SUBSTATE			<=	1;
+			end			
+			if(SUBSTATE==1)begin				
+				LCD_E				<=	1'b1;					//Enable Bus		
+				LCD_DB 			<= DATA;						//WRITE THE CHARACTER
+				if(!flag_250ns) begin							//WAIT at least 250ns (required for LCD_E)
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 				
+					SUBSTATE		<=	SUBSTATE+1;				//Go to next SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+			if(SUBSTATE==2)begin
+				LCD_E				<=	1'b0;						//Disable Bus, Triggers LCD to read BUS
+				LCD_DB 			<= LCD_DB;						//Keep Data Valid
+				if(!flag_250ns) begin							//WAIT at least 250ns (required for LCD_E)
+					STATE			<=	STATE;					//Maintain current STATE
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 		
+					STATE			<=	15;//STATE+1;			//Go to next STATE
+					SUBSTATE		<=	0;						//Reset SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end		
+		end	
+		//---------------------------------------------------------------------------------------
+		10: begin//----------------------- WRITE INSTRUCTION ------------------------------------
+			LCD_RS				<=	1'b0;						//Indicate an instruction is to be sent soon
+			LCD_RW				<=	1'b0;						//Indicate a write operation	
+			RDY					<= 	1'b0;						//Indicate that the module is busy
+			if(SUBSTATE==0)begin	 		
+				LCD_E				<=	1'b0;						//Disable Bus
+				LCD_DB 				<=	LCD_DB;					//Maintain Previous Data on the Bus
+				STATE				<=	STATE;				
+				SUBSTATE			<=	1;
+			end			
+			if(SUBSTATE==1)begin				
+				LCD_E				<=	1'b1;						//Enable Bus		
+				LCD_DB 			<= DATA;						//Data Valid
+				if(!flag_250ns) begin						//WAIT at least 250ns (required for LCD_E)
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 				
+					SUBSTATE		<=	SUBSTATE+1;				//Go to next SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+			if(SUBSTATE==2)begin
+				LCD_E				<=	1'b0;						//Disable Bus, Triggers LCD to read BUS
+				LCD_DB 			<= LCD_DB;					//Keep Data Valid
+				if(!flag_42us) begin							//WAIT at least 49us (required for operation to process)
+					STATE			<=	STATE;					//Maintain current STATE
+					SUBSTATE		<=	SUBSTATE;				//Maintain current SUBSTATE
+					flag_rst		<=	1'b0; 					//Start or Continue counting									
+				end
+				else begin 		
+					STATE			<=	15;//STATE+1;			//Go to next STATE
+					SUBSTATE		<=	0;							//Reset SUBSTATE
+					flag_rst		<=	1'b1; 					//Stop counting					
+				end
+			end
+		end
+		//---------------------------------------------------------------------------------------
+		default: begin//----------This is the IDLE STATE, DO NOTHING UNTIL OPER is set-----------
+			LCD_RS	<=		LCD_RS;								//Indicate an instruction is to be sent soon
+			LCD_RW	<= 	1'b0;									//Indicate a write operation
+			LCD_DB 	<= 	LCD_DB;								//Maintain Data Bus
+			LCD_E		<=		1'b0;									//Disable Bus
+			RDY		<=		1'b1;									//Indicate that the system is ready to taking in data
+			if(ENB==1 && RST==0)begin
+				case(OPER)
+					0:STATE<=STATE; 	//IDLE
+					1:STATE<=10;		//WRITE CHARACTER
+					2:STATE<=11;		//WRITE INSTRUCTION (assumes 49us or less time to process instr)
+					3:STATE<=0;			//RESET
+				endcase
+			end
+			else if(RST==1)begin
+				STATE<=0;
+			end
+		end
+	endcase
+end
+endmodule
+
